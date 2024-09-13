0e3027f5f803a5160566f872c71c7f07ca6b4f1c
David Tran <tranhuyducseven@gmail.com>
2024-09-13 08:08:55 +0700

    fix: nextconfig

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,12 +6,11 @@ set -Ex
 
 env_vars="NEXT_PUBLIC_APP_URL NEXT_PUBLIC_STATUS_MESSAGE NEXT_PUBLIC_ENVIRONMENT NEXT_PUBLIC_AXELARSCAN_API_URL NEXT_PUBLIC_VALIDATOR_API_URL NEXT_PUBLIC_TOKEN_TRANSFER_API_URL NEXT_PUBLIC_GMP_API_URL NEXT_PUBLIC_RPC_URL NEXT_PUBLIC_LCD_URL NEXT_PUBLIC_GTM_ID NEXT_PUBLIC_GA_TRACKING_ID NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID NEXT_PUBLIC_DEFAULT_TITLE NEXT_PUBLIC_DEFAULT_DESCRIPTION NEXT_PUBLIC_CUSTOM_EVM_CHAINS"
 
-
 function apply_path {
     # Replace placeholders in /app/dist directory
     for var in $env_vars; do
         echo "Replacing ${var} with ${!var}"
-        sed -i "s#${var}=.*#${var}=${!var}#g" /app/.env
+        find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_${var}#${!var}#g"
     done
 }
 
